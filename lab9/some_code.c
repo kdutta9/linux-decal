4424b820c4f3f41cfdd03f00214249e2b29cb39a
Kunal Dutta <kdutta@berkeley.edu>
2019-11-05 20:40:32 -0800

    Added my new feature

--- lab9/some_code.c
+++ lab9/some_code.c
@@ -4,7 +4,8 @@ int main(int argc, char** argv) {
     char* x = "Correct output";
     puts(x);
 
-    /* TODO: add new feature */
+    char* feature = "Here's my new feature!";
+    puts(feature);
 
     return 0;
 }
